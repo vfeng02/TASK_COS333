a306fdef91ade94f932404950c85723352f57a3a
Andres Blanco Bonilla <afbb@princeton.edu>
2022-10-13 19:13:48 -0400

    added db code attempt

---
+++ TASKdbcode/clear_task_db.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# delete_task_db.py
+# Author: Andres Blanco Bonilla
+# Uses SQLAlchemy to clear the demographic database in ElephantSQL
+# (deleting all the table data but not the tables themselves)
+# for testing purposes
+#-----------------------------------------------------------------------
+
+import sys
+
+import sqlalchemy
+import sqlalchemy.orm
+from sqlalchemy import func
+import psycopg2
+
+import demographic_db as database
+
+#-----------------------------------------------------------------------
+
+DATABASE_URL = "postgresql+psycopg2://usqmchwx:jVw_QrUQ-blJpl1dXhixIQmPAsD89W-R@peanut.db.elephantsql.com/usqmchwx"
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+
+def main():
+
+    if len(sys.argv) != 1:
+        print('Usage: python create.py', file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        engine = sqlalchemy.create_engine(DATABASE_URL)
+        database.Base.metadata.drop_all(engine)
+        database.Base.metadata.create_all(engine)
+        engine.dispose()
+
+    except Exception as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
