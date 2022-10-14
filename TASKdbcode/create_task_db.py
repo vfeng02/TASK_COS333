d7e18e4dc82dbd0fb9fdb72e83b002374876257a
Andres Blanco Bonilla <afbb@princeton.edu>
2022-10-14 15:57:58 -0400

    updated demographic options and meal site options

--- TASKdbcode/create_task_db.py
+++ TASKdbcode/create_task_db.py
@@ -71,55 +71,6 @@ def insert_users(session):
 
     #-------------------------------------------------------------------
 
-    # first_baptist = database.First_Baptist_Church()
-    # session.add(first_baptist)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
-    # trinity = database.Trinity_Episcopal_Cathedral()
-    # session.add(trinity)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
-    # first_united = database.\
-        # First_United_Methodist_Church_of_Hightstown()
-    # session.add(first_united)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
-    # first_pres = database.First_Presbyterian_Church_of_Hightstown()
-    # session.add(first_pres)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
-    # princeton_united = database.Princeton_United_Methodist_Church()
-    # session.add(princeton_united)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
-    # holy_apostles = database.Holy_Apostles_Episcopal_Church()
-    # session.add(holy_apostles)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
-    # trenton_rescue = database.Trenton_Rescue_Mission()
-    # session.add(trenton_rescue)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
-    # medallion_care = database.Medallion_Care_Behavioral_Health()
-    # session.add(medallion_care)
-    # session.commit()
-
-    #-------------------------------------------------------------------
-
 
 def main():
 
