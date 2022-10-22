a15babaa9e535266aa30f4afb380bf2582a8e1c0
Vicky Feng <vyfeng@princeton.edu>
2022-10-22 16:58:23 -0400

    patron data

--- TASKfrontend/task.py
+++ TASKfrontend/task.py
@@ -1,10 +1,19 @@
 import time
 import flask
+import sys
+# from demographic_db import add_patron
+# import os
+# import urllib.parse as up
+# import psycopg2
+# import contextlib
 # import demographic_db
 
 #-----------------------------------------------------------------------
 
 app = flask.Flask(__name__, template_folder='templates')
+# sys.path.insert(0, '../TASKdbcode')
+# sys.path.insert(0, '/Users/vicky/Desktop/COS333/TASK_COS333/Taskdbcode')
+# DATABASE_URL = 'postgres://usqmchwx:jVw_QrUQ-blJpl1dXhixIQmPAsD89W-R@peanut.db.elephantsql.com/usqmchwx'
 
 #-----------------------------------------------------------------------
 
@@ -55,8 +64,9 @@ def submit_result():
         prev_name = '(None)'
     else:
         prev_name = name
+
+    # add_patron()
         # submit name to the db
-        # books = demographic_db.get_books(author) # Exception handling omitted
 
     html_code = flask.render_template('submitresult.html',
         ampm=get_ampm(),
@@ -65,3 +75,27 @@ def submit_result():
     response = flask.make_response(html_code)
     response.set_cookie('prev_name', prev_name)
     return response
+
+ #-----------------------------------------------------------------------
+
+# def submit_data():
+#     try:
+#         up.uses_netloc.append("postgres")
+#         url = up.urlparse(os.environ["DATABASE_URL"])
+#         with psycopg2.connect(database=url.path[1:],
+#             user=url.username,
+#             password=url.password,
+#             host=url.hostname,
+#             port=url.port
+#             ) as connection:
+#                 with contextlib.closing(connection.cursor()) as cursor:
+#                     cursor.execute("PRAGMA case_sensitive_like=OFF")
+#                     stmt_str ="SELECT classid, dept, coursenum, area, title"
+#                     stmt_str += " FROM courses, classes, crosslistings "
+#                     stmt_str += "WHERE classes.courseid = courses.courseid "
+#                     stmt_str += "AND courses.courseid = "
+#                     stmt_str += "crosslistings.courseid "
+#                     arr = []
+#     except Exception as ex:
+#         #print(sys.argv[0] + ": " + str(ex), file=sys.stderr)
+#         return False, None
