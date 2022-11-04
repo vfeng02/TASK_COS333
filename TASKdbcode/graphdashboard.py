74069020d5575f9e3d6a7053b7c2326a01b999f6
Andres Blanco Bonilla <afbb@princeton.edu>
2022-11-03 21:40:57 -0400

    fixed spacing on charts a little

--- TASKdbcode/graphdashboard.py
+++ TASKdbcode/graphdashboard.py
@@ -78,12 +78,14 @@ def init_callbacks(graph_app):
             specs = [[{'type':'domain'}], [{'type':'domain'}]]
             pie_charts = make_subplots(rows=2, cols=1, specs=specs)
             single_chart = go.Pie(values = summary_counts, labels = summary_counts.index,
-                            title = "Races of Patrons at All Meal Sites", legendgroup=1)
+                                  title = f"Races of Single Race Patrons at {select_d1} Meal Site",
+                                  legendgroup=1)
             other_chart = go.Pie(values = multi_counts, labels = multi_counts.index,
-                            title = "Other: Races of Multi-Racial Patrons at All Meal Sites", legendgroup=2)
+                            title = f"Other: Races of Multi-Racial Patrons at {select_d1} Meal Site", legendgroup=2)
             pie_charts.add_trace(single_chart, 1, 1)
             pie_charts.add_trace(other_chart, 2, 1)
-            pie_charts.update_layout(legend_tracegroupgap = 180)
+            # This is how many pixels apart to put the 2 charts
+            pie_charts.update_layout(legend_tracegroupgap = 120)
             pie_charts.update_layout(title=f"Races of Patrons at {select_d1} Meal Site")
             return pie_charts
         else:
