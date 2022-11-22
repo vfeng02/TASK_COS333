1a80c3b9f87cb3700944cfe2af0a84204afa87b1
Andres Blanco Bonilla <afbb@princeton.edu>
2022-11-22 16:46:22 -0500

    added simple user display

---
+++ udisplay.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# udisplay.py
+# Author: Andres Blanco Bonilla
+# Prints the contents of all the meal site tables in the TASK database
+#-----------------------------------------------------------------------
+
+from TASKdbcode import demographic_db
+
+def main():
+    # demographic_db.add_user({"username": "thewaitress",
+    #                          "password": "Waitress' password",
+    #                          "email": "waitress@fx.com",
+    #                          "role": "representative"})
+    demographic_db.display_users()
+
+#-----------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
