9172de6f4a6344d5ff653821ddeab57f0c4321aa
Vicky Feng <vyfeng@princeton.edu>
2022-12-05 21:10:15 -0500

    pycache

--- TASKdbcode/demographic_db.py
+++ TASKdbcode/demographic_db.py
@@ -15,7 +15,7 @@ from sqlalchemy.sql import functions
 from TASKdbcode import database_constants
 # sql_alchemy filters has to be downloaded from this repo
 # https://github.com/bodik/sqlalchemy-filters
-from sqlalchemy_filters import apply_filters
+from sqlalchemy_filters import filters
 # from TASKdbcode import database_constants
 
 from werkzeug.security import generate_password_hash,\
@@ -191,7 +191,7 @@ def get_patrons(filter_dict = {}, select_fields = []):
                 else:
                     filter_spec = {"field": key, "op" : "==", "value": value}
                     
-                query = apply_filters(query, filter_spec)
+                query = filters.apply_filters(query, filter_spec)
             # print(query)
             demographic_df = pandas.read_sql(query.statement, session.bind)
 
@@ -235,7 +235,7 @@ def filter_dms(filter_dicts):
                     else:
                         filter_spec = filter_dict
                         
-                    query = apply_filters(query, filter_spec)
+                    query = filters.apply_filters(query, filter_spec)
             demographic_df = pandas.read_sql(query.statement, session.bind)
         # engine.dispose()
         return demographic_df
