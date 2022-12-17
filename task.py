6038856260c02b99e648e3439b93f39403c37fb1
Vicky Feng <vyfeng@princeton.edu>
2022-12-16 22:03:52 -0500

    admin login error style

--- task.py
+++ task.py
@@ -36,7 +36,20 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__, template_folder='templates')
 messages = {
-    'auth_error': 'You are not authorized as an administrator. Please return to the previous page and enter administrator login details.'
+    'auth_error': '''
+    <div class="container-fluid" style ="width: 100%; height: 100%; background-color: #145078">
+    <div class="container-fluid rounded" style ="position: absolute; color: white; width: 50%; margin-top: 80px; margin-left: 25%; padding: 10px;
+    border: 4px solid #ff9f46" >
+    <center>
+    <h3 style="color: white">
+    You are not authorized as an administrator. 
+    Please return to <a href="https://task-dk40.onrender.com/" style="color:#ff9f46">the previous page</a> and enter administrator login details. 
+    </h3>
+    <center>
+    </div>
+    </div>
+    
+    '''
 }
 csrf = CSRFProtect()
 csrf._exempt_views.add('dash.dash.dispatch')
